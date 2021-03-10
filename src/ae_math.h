3f4ed47f66133ae49c4be6b6c5ac37e5fd726633
snaulX <alexandergunger666@gmail.com>
2021-03-10 14:01:47 +0300

    Move code from Experimental branch of Alpha Engine

---
+++ src/ae_math.h
@@ -0,0 +1,21 @@
+#ifdef __cplusplus
+namespace AlphaEngine { namespace Math
+{
+extern "C" 
+{
+#endif
+
+	struct Vector2
+	{
+		float x, y;
+	};
+
+	struct Vector3
+	{
+		float x, y, z;
+	};
+	
+#ifdef __cplusplus
+} // end of extern "C"
+} } // end of namespace
+#endif
